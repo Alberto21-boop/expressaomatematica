753e85b0bb4d329a2525c7b0f67a4d777b2bde27
Alberto <albertobarbosa0003@gmail.com>
2022-09-04 16:28:06 -0300

    Adicionar arquivos de projeto.

---
+++ expressaomatematica/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace expressaomatematica;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //1) Resolva a expressão numérica: 30 ÷ [10 + (2 + 3)]
+
+        Console.WriteLine("vamos resolver a expressão numérica: 30 ÷ [10 + (2 + 3)]");
+
+        int resposta;
+
+        resposta = 30 / (10 + (2 + 3));
+
+        Console.WriteLine("O resultado é = " + resposta);
+    }
+}
